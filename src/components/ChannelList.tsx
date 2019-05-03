424c25a39c26346eb8e0b7db788a48092d7c78cd
take8 <37664118+take8@users.noreply.github.com>
2019-05-03 15:06:29 +0900

    チャンネル一覧ComponentをFunctionalComponentに変更

--- src/components/ChannelList.tsx
+++ src/components/ChannelList.tsx
@@ -5,25 +5,28 @@ import { Link, NavLink } from "react-router-dom";
 // TODO: 簡略化のため、チャンネル一覧をここで定義
 const channels = [ "general", "random" ];
 
-export class ChannelList extends React.Component<{}, {}> {
-  public render() {
-    return (
-      <Menu inverted vertical fixed={'left'}>
-        <Menu.Item>
-          Channels
-          <Menu.Menu>
-            {channels.map(channel =>
-              // 配列を扱う場合は必ず key を指定する
-              <Menu.Item
-                  key={channel}
-                  as={NavLink}
-                  to={{ pathname: `/channels/${channel}` }}>
-                {channel}
-              </Menu.Item>
-            )}
-          </Menu.Menu>
-        </Menu.Item>
-      </Menu>
-    );
-  }
-}
+export const ChannelList: React.FC = () => {
+  return (
+    <Menu inverted vertical fixed={'left'}>
+      <Menu.Item as={Link} to={'/'}>
+        Home
+        <Icon name='home' />
+      </Menu.Item>
+      <Menu.Item>
+        Channels
+        <Icon name='list' />
+        <Menu.Menu>
+          {channels.map(channel =>
+            // 配列を扱う場合は必ず key を指定する
+            <Menu.Item
+                key={channel}
+                as={NavLink}
+                to={{ pathname: `/channels/${channel}` }}>
+              {channel}
+            </Menu.Item>
+          )}
+        </Menu.Menu>
+      </Menu.Item>
+    </Menu>
+  );
+};
